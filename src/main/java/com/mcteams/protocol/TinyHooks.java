619bfd6851133c019da491f2d2929eafac3c3cb5
Ryan W <rbrickproductions123@gmail.com>
2024-02-27 21:00:28 -0600

    initial commit

---
+++ src/main/java/com/mcteams/protocol/TinyHooks.java
@@ -0,0 +1,17 @@
+package com.mcteams.protocol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TinyHooks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
