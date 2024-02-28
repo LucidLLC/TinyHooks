32494e556b082ea0fe6312cf0d8c07815d8866ea
Ryan W <rbrickproductions123@gmail.com>
2024-02-27 21:48:49 -0600

    Simple implementation of TinyHooks

--- src/main/java/com/mcteams/protocol/TinyHooks.java
+++ src/main/java/com/mcteams/protocol/TinyHooks.java
@@ -1,17 +1,117 @@
 package com.mcteams.protocol;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import com.comphenix.tinyprotocol.TinyProtocol;
+import io.netty.channel.Channel;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
-public final class TinyHooks extends JavaPlugin {
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+public final class TinyHooks extends TinyProtocol {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+
+    enum Result {
+        PASS, CANCEL
+    }
+
+    enum Side {
+        // Packet<Stage>In
+        IN,
+
+        // Packet<Stage>Out
+        OUT
+    }
+
+    enum Priority {
+        LOWEST,
+        LOW,
+        DEFAULT,
+        HIGH,
+        HIGHEST
+    }
+
+    public final Map<Side, Map<Priority, Map<Class<?>, Set<BiFunction<Object, Player, Result>>>>> hooks = new ConcurrentHashMap<>();
+
+    public TinyHooks(Plugin plugin) {
+        super(plugin);
+    }
+
+    public <T> void addPacketHook(Side side, Priority priority, Class<? extends T> packet, BiFunction<T, Player, Result> on) {
+        hooks.computeIfAbsent(side, (s) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(priority, (p) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(packet, (c) -> new HashSet<>()).add((BiFunction<Object, Player, Result>) on);
 
     }
 
+    public <T> void addPacketHook(Side side, Class<? extends T> packet, BiFunction<T, Player, Result> on) {
+        hooks.computeIfAbsent(side, (s) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(Priority.DEFAULT, (p) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(packet, (c) -> new HashSet<>()).add((BiFunction<Object, Player, Result>) on);
+    }
+
+    public <T> void addIncomingPacketHook(Priority priority, Class<? extends T> packet, BiFunction<T, Player, Result> on) {
+        hooks.computeIfAbsent(Side.IN, (s) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(priority, (p) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(packet, (c) -> new HashSet<>()).add((BiFunction<Object, Player, Result>) on);
+    }
+
+    public <T> void addIncomingPacketHook(Class<? extends T> packet, BiFunction<T, Player, Result> on) {
+        hooks.computeIfAbsent(Side.IN, (s) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(Priority.DEFAULT, (p) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(packet, (c) -> new HashSet<>()).add((BiFunction<Object, Player, Result>) on);
+    }
+
+    public <T> void addOutgoingPacketHook(Priority priority, Class<? extends T> packet, BiFunction<T, Player, Result> on) {
+        hooks.computeIfAbsent(Side.OUT, (s) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(priority, (p) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(packet, (c) -> new HashSet<>()).add((BiFunction<Object, Player, Result>) on);
+    }
+
+    public <T> void addOutgoingPacketHook(Class<? extends T> packet, BiFunction<T, Player, Result> on) {
+        hooks.computeIfAbsent(Side.OUT, (s) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(Priority.DEFAULT, (p) -> new ConcurrentHashMap<>())
+                .computeIfAbsent(packet, (c) -> new HashSet<>()).add((BiFunction<Object, Player, Result>) on);
+    }
+
+
+    private Object processHooks(Side side, Player player, Object packet) {
+        Map<Priority, Map<Class<?>, Set<BiFunction<Object, Player, Result>>>> packetInHooks =
+                hooks.computeIfAbsent(side, (s) -> new ConcurrentHashMap<>());
+
+        if (packetInHooks.isEmpty()) return packet;
+
+        Result result = Result.PASS;
+
+        for (Priority value : Priority.values()) {
+            Map<Class<?>, Set<BiFunction<Object, Player, Result>>> prioritizedHooks = packetInHooks
+                    .computeIfAbsent(value, priority -> new ConcurrentHashMap<>());
+
+            if (prioritizedHooks.isEmpty()) continue;
+            Set<BiFunction<Object, Player, Result>> hooks = prioritizedHooks.getOrDefault(packet.getClass(), new HashSet<>());
+            if (hooks.isEmpty()) continue;
+
+            for (BiFunction<Object, Player, Result> hook : hooks) {
+                result = hook.apply(packet, player);
+            }
+        }
+
+        if (result == Result.CANCEL) {
+            return null;
+        }
+
+        return packet;
+    }
+
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public Object onPacketInAsync(Player sender, Channel channel, Object packet) {
+        return processHooks(Side.IN, sender, packet);
+    }
+
+    @Override
+    public Object onPacketOutAsync(Player receiver, Channel channel, Object packet) {
+        return processHooks(Side.OUT, receiver, packet);
     }
 }
